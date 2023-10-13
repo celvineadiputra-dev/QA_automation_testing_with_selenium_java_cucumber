d6c010683c8e2be58e5113c0bf8eb5f09d8eb5af
celvine adi putra <celvineadiputra2@gmail.com>
2023-10-13 19:53:31 +0700

    Init : [Project]

---
+++ src/main/java/com/celvine/th/Main.java
@@ -0,0 +1,8 @@
+package com.celvine.th;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HomeWork 06 Automation Testing");
+        System.out.println("Name : Celvine Adi Putra");
+    }
+}
